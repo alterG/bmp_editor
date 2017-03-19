e5a6cbd3c909eaea3879e6ae9cdf465c096655d9
alterG <alterg@niuitmo.ru>
2017-03-19 22:41:03 +0300

    fixed rotating for small pictures like 3*1px

--- src/bmp_rotate.c
+++ src/bmp_rotate.c
@@ -13,26 +13,12 @@ static pixel_t * pixel_t_get (const image_t * const image, const uint32_t index_
 }
 
 
-static void pixel_t_set
-(image_t * const image, const pixel_t pixel_replacer, const uint32_t index_x, const uint32_t index_y) {
-	
-	*(image->rastr+index_y*(image->width)+index_x) = pixel_replacer;
-	printf("%d %d %d\n", pixel_replacer.rgbtBlue, pixel_replacer.rgbtGreen, pixel_replacer.rgbtRed);
-
-
-/*	pixel_t * pixel_to_replace;
-	pixel_to_replace = &
-	
-	*pixel_to_replace = pixel_replacer;*/
-}
-
-
 /* Function rotates image clockwise on 90 degrees */
 
 void image_t_rotated_get (const image_t * const input_image, image_t * const output_image) {
 	 
 	uint32_t x1, y1, x2, y2;	/* (x2,y2) is new coordinates of (x1,y1) */
-	int i, j, k;
+	int i, j;
 	
 	output_image->height = input_image->width;
 	output_image->width = input_image->height;
@@ -43,9 +29,17 @@ void image_t_rotated_get (const image_t * const input_image, image_t * const out
 			x1 = j;
 			y1 = i;
 			x2 = y1;
-			y2 = (input_image->width) - x1;
+			y2 = (input_image->width) - x1-1;
 			*(pixel_t_get(output_image, x2, y2)) = *(pixel_t_get(input_image, x1, y1));
-			/*printf("%d:  %d %d %d\n",i, (*(pixel_t_get(output_image, x2, y2))).rgbtBlue, (*(pixel_t_get(output_image, x2, y2))).rgbtGreen, (*(pixel_t_get(output_image, x2, y2))).rgbtRed);*/
+			printf("input: h_%d w_%d color %d %d %d\noutput: h_%d w_%d color %d %d %d\n",
+				y1, x1,			
+				(pixel_t_get(input_image, x1, y1))->rgbtRed,
+				(pixel_t_get(input_image, x1, y1))->rgbtGreen,
+				(pixel_t_get(input_image, x1, y1))->rgbtBlue,
+				y2, x2,
+				(pixel_t_get(output_image, x2, y2))->rgbtRed,
+				(pixel_t_get(output_image, x2, y2))->rgbtGreen,				
+				(pixel_t_get(output_image, x2, y2))->rgbtBlue);
 		}
 	}
 }
